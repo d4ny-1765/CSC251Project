9a330a87c222373219eeaca0b422d41e831c0b05
d4ny-1765 <d4ny.1765@gmail.com>
2023-10-02 19:55:28 +0400

    Update Project_Dany_Babikerali.java

--- Project_Dany_Babikerali.java
+++ Project_Dany_Babikerali.java
@@ -1,65 +1,19 @@
-import java.util.*; 
-import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
 public class Project_Dany_Babikerali
 {
    public static void main(String[] args)
    {
-               //Defining The Scanner Class
-               //Scanner scnr = new Scanner(System.in);
-               
-               //Requesting inputs
-      //System.out.print("Enter the Policy Number: ");
-               //String number = scnr.nextLine();
-               //scnr.nextLine();
-               
-               //System.out.println();
-              
-               //System.out.print("Enter the Provier's Name: ");
-               //String name = scnr.nextLine();
-               
-               //System.out.println();
-               
-               //System.out.print("Enter your First Name: ");
-               //String fName = scnr.nextLine();
-               
-               //System.out.println();
-               
-               //System.out.print("Enter your Last Name: ");
-               //String lName = scnr.nextLine();
-              
-               //System.out.println();
-               
-               //System.out.print("Enter your age: ");
-               //int age = scnr.nextInt();
-               //scnr.nextLine();
-               
-               //System.out.println();
-               
-               //System.out.print("Do you smoke? (type 'smoker' if yes): ");
-               //String smokeStat = scnr.nextLine();
-               
-               //System.out.println();
-               
-               //System.out.print("Enter your height (in inches): ");
-               //double height = scnr.nextDouble();
-               //scnr.nextLine();
-               
-               //System.out.println();
-               
-               //System.out.print("Enter your weight (in pounds): ");
-               //double weight = scnr.nextDouble();
-               //scnr.nextLine();
-               
-               //System.out.println();
-      
-      //Defining the Policy Class
-      //Policy p = new Policy(number, name, fName, lName, age, smokeStat, height, weight);
-      
+                    
      try { 
       File file = new File("PolicyInformation.txt");
       Scanner inputFile = new Scanner(file);
       
+      //Creating an arrayList
+      ArrayList<Policy> policyList = new ArrayList<>();
       
       int noOfSmokers = 0;
       int noOfNonS = 0;
@@ -86,13 +40,14 @@ public class Project_Dany_Babikerali
             inputFile.nextLine();
          }
          
-         Policy policy = new Policy(pNumber, pName, pfName, plName, pAge, sStatus, pHeight, pWeight);
          if (sStatus == "smoker") {
             noOfSmokers += 1;
          }
          if (sStatus == "non-smoker") {
             noOfNonS += 1;
          }
+         
+         policyList.add(new Policy(pNumber, pName, pfName, plName, pAge, sStatus, pHeight, pWeight));
        }
       System.out.println(file);
      }
@@ -101,6 +56,8 @@ public class Project_Dany_Babikerali
          //use the getMessage method of the exception we "caught" to print out it's message about what went wrong
          System.out.println("Something went wrong reading the file: " + ex.getMessage());
      }
+   for(Policy policy : policyList) {
+      
 
       System.out.println("Policy Number: " + policy.getNumber());           
       System.out.println("Provider Name: " + policy.getName());
@@ -112,18 +69,9 @@ public class Project_Dany_Babikerali
       System.out.println("Policyholder’s Weight: " + policy.getWeight());
       System.out.println("Policyholder’s BMI: " + String.format("%.2f", policy.getBMI()));
       System.out.println("Policy Price: $" + String.format("%.2f", policy.getPrice()));
-      
-      //Displaying the Information 
-      
-      //System.out.println("Policy Number: " + p.getNumber());           
-      //System.out.println("Provider Name: " + p.getName());
-      //System.out.println("Policyholder’s First Name: " + p.getfName());
-      //System.out.println("Policyholder’s Last Name: " + p.getlName());
-      //System.out.println("Policyholder’s Age: " + p.getAge());
-      //System.out.println("Policyholder’s Smoking Status: " + p.getSmokeStatus());
-      //System.out.println("Policyholder’s Height: " + p.getHeight());
-      //System.out.println("Policyholder’s Weight: " + p.getWeight());
-      //System.out.println("Policyholder’s BMI: " + String.format("%.2f", p.getBMI()));
-      //System.out.println("Policy Price: $" + String.format("%.2f", p.getPrice()));
+      System.out.println("The number of policies with a smoker is: " + noOfSmokers);
+      System.out.println("The number of policies with a smoker is: " + noOfNonS);
+    }  
+
 	}
 }
