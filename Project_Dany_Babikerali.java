526739f0323ddb10e036d15e3f827847f17e72d8
d4ny-1765 <d4ny.1765@gmail.com>
2023-10-02 19:14:27 +0400

    Update Project_Dany_Babikerali.java
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Dany_Babikerali.java
+++ Project_Dany_Babikerali.java
@@ -6,63 +6,63 @@ public class Project_Dany_Babikerali
    public static void main(String[] args)
    {
                //Defining The Scanner Class
-               Scanner scnr = new Scanner(System.in);
+               //Scanner scnr = new Scanner(System.in);
                
                //Requesting inputs
-               System.out.print("Enter the Policy Number: ");
-               String number = scnr.nextLine();
-               scnr.nextLine();
+      //System.out.print("Enter the Policy Number: ");
+               //String number = scnr.nextLine();
+               //scnr.nextLine();
                
-               System.out.println();
-               
-               System.out.print("Enter the Provier's Name: ");
-               String name = scnr.nextLine();
-               
-               System.out.println();
-               
-               System.out.print("Enter your First Name: ");
-               String fName = scnr.nextLine();
-               
-               System.out.println();
-               
-               System.out.print("Enter your Last Name: ");
-               String lName = scnr.nextLine();
+               //System.out.println();
               
-               System.out.println();
+               //System.out.print("Enter the Provier's Name: ");
+               //String name = scnr.nextLine();
                
-               System.out.print("Enter your age: ");
-               int age = scnr.nextInt();
-               scnr.nextLine();
+               //System.out.println();
                
-               System.out.println();
+               //System.out.print("Enter your First Name: ");
+               //String fName = scnr.nextLine();
                
-               System.out.print("Do you smoke? (type 'smoker' if yes): ");
-               String smokeStat = scnr.nextLine();
+               //System.out.println();
                
-               System.out.println();
+               //System.out.print("Enter your Last Name: ");
+               //String lName = scnr.nextLine();
+              
+               //System.out.println();
                
-               System.out.print("Enter your height (in inches): ");
-               double height = scnr.nextDouble();
-               scnr.nextLine();
+               //System.out.print("Enter your age: ");
+               //int age = scnr.nextInt();
+               //scnr.nextLine();
                
-               System.out.println();
+               //System.out.println();
                
-               System.out.print("Enter your weight (in pounds): ");
-               double weight = scnr.nextDouble();
-               scnr.nextLine();
+               //System.out.print("Do you smoke? (type 'smoker' if yes): ");
+               //String smokeStat = scnr.nextLine();
                
-               System.out.println();
+               //System.out.println();
+               
+               //System.out.print("Enter your height (in inches): ");
+               //double height = scnr.nextDouble();
+               //scnr.nextLine();
+               
+               //System.out.println();
+               
+               //System.out.print("Enter your weight (in pounds): ");
+               //double weight = scnr.nextDouble();
+               //scnr.nextLine();
+               
+               //System.out.println();
       
       //Defining the Policy Class
-      Policy p = new Policy(number, name, fName, lName, age, smokeStat, height, weight);
+      //Policy p = new Policy(number, name, fName, lName, age, smokeStat, height, weight);
       
      try { 
       File file = new File("PolicyInformation.txt");
       Scanner inputFile = new Scanner(file);
       
       
-      noOfSmokers = 0;
-      noOfNonS = 0;
+      int noOfSmokers = 0;
+      int noOfNonS = 0;
       String pNumber = "";
       String pName = "";
       String pfName = "";
@@ -115,15 +115,15 @@ public class Project_Dany_Babikerali
       
       //Displaying the Information 
       
-      System.out.println("Policy Number: " + p.getNumber());           
-      System.out.println("Provider Name: " + p.getName());
-      System.out.println("Policyholder’s First Name: " + p.getfName());
-      System.out.println("Policyholder’s Last Name: " + p.getlName());
-      System.out.println("Policyholder’s Age: " + p.getAge());
-      System.out.println("Policyholder’s Smoking Status: " + p.getSmokeStatus());
-      System.out.println("Policyholder’s Height: " + p.getHeight());
-      System.out.println("Policyholder’s Weight: " + p.getWeight());
-      System.out.println("Policyholder’s BMI: " + String.format("%.2f", p.getBMI()));
-      System.out.println("Policy Price: $" + String.format("%.2f", p.getPrice()));
+      //System.out.println("Policy Number: " + p.getNumber());           
+      //System.out.println("Provider Name: " + p.getName());
+      //System.out.println("Policyholder’s First Name: " + p.getfName());
+      //System.out.println("Policyholder’s Last Name: " + p.getlName());
+      //System.out.println("Policyholder’s Age: " + p.getAge());
+      //System.out.println("Policyholder’s Smoking Status: " + p.getSmokeStatus());
+      //System.out.println("Policyholder’s Height: " + p.getHeight());
+      //System.out.println("Policyholder’s Weight: " + p.getWeight());
+      //System.out.println("Policyholder’s BMI: " + String.format("%.2f", p.getBMI()));
+      //System.out.println("Policy Price: $" + String.format("%.2f", p.getPrice()));
 	}
 }
